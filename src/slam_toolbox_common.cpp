6484c6117c9952914a8e951517ef88c2a527ed0a
MikeDegany <mike.degany@gmail.com>
2026-01-26 00:03:43 -0600

    chore: compatible with ros2 humble

--- src/slam_toolbox_common.cpp
+++ src/slam_toolbox_common.cpp
@@ -498,9 +498,12 @@ void SlamToolbox::setROSInterfaces()
   loop_closure_event_pub_ = this->create_publisher<slam_toolbox::msg::LoopClosureEvent>(
     "slam_toolbox/loop_closure_event", 10);
 
+  // scan_filter_sub_ =
+  //   std::make_unique<message_filters::Subscriber<sensor_msgs::msg::LaserScan>>(
+  //   shared_from_this().get(), scan_topic_, rclcpp::SensorDataQoS());
   scan_filter_sub_ =
-    std::make_unique<message_filters::Subscriber<sensor_msgs::msg::LaserScan>>(
-    shared_from_this().get(), scan_topic_, rclcpp::SensorDataQoS());
+      std::make_unique<message_filters::Subscriber<sensor_msgs::msg::LaserScan, rclcpp_lifecycle::LifecycleNode>>(
+      this, scan_topic_, rclcpp::SensorDataQoS().get_rmw_qos_profile());
   scan_filter_ =
     std::make_unique<tf2_ros::MessageFilter<sensor_msgs::msg::LaserScan>>(
     *scan_filter_sub_, *tf_, odom_frame_, scan_queue_size_,
